2cace588509f3053ec99cc0a08cbc124a707e3a8
CHELSEA DOLE <chelseadole@gmail.com>
2017-10-19 15:25:22 -0700

    finished successful create_dict test function

--- src/test_trigram.py
+++ src/test_trigram.py
@@ -1,17 +1,17 @@
 """Tests for trigrams, a program that generates text based on a source text."""
 
-DICT_KEYS = [
-    ('there blazed', ['forth', 'the']),
-    ('kept down', ['by', 'in']),
-    ('now got', ['up', 'some']),
-    ('A singular', ['change']),
-    ('at first', ['had'])
-]
+DICT_KEYS = {
+    'there blazed': ['forth', 'the'],
+    'kept down': ['by', 'in'],
+    'now got': ['up', 'some'],
+    'A singular': ['change,'],
+    'at first': ['had']
+}
 
 
-def test_create_dict(n=50, path='testpoe.txt'):
+def test_create_dict(path='testpoe.txt'):
     """Test that trigrammer outputs actual trigram text."""
     from trigram import create_dict
-    trigram_dict = create_dict(50, 'testpoe.txt')
+    trigram_dict = create_dict('testpoe.txt')
     for key in DICT_KEYS:
         assert key in trigram_dict and trigram_dict[key] == DICT_KEYS[key]
