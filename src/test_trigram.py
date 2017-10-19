8bf74ec95144a2cc4712f771fa6b736e46dc3db8
CHELSEA DOLE <chelseadole@gmail.com>
2017-10-19 15:50:40 -0700

    test-trigram changes

--- src/test_trigram.py
+++ src/test_trigram.py
@@ -15,3 +15,16 @@ def test_create_dict(path='testpoe.txt'):
     trigram_dict = create_dict('testpoe.txt')
     for key in DICT_KEYS:
         assert key in trigram_dict and trigram_dict[key] == DICT_KEYS[key]
+
+
+def test_get_first_key_words(path='testpoe.txt'):
+    from trigram import get_first_key_words
+    from trigram import create_dict
+    created_dict = create_dict(path)
+    first_key = get_first_key_words(path, created_dict)
+    assert first_key in created_dict
+
+
+def test_trigrammer(n=50, path='testpoe.txt'):
+    from trigram import trigrammer
+
